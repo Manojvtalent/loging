ea1ac1fcd501c99cc59b7a93a5ba61906f857960
manoj goud kurmindla <manojgoud.vtalent@gmail.com>
2024-02-17 12:55:11 +0530

    mmm

---
+++ providers/appAuthorizationServerProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+
+namespace WebApplication.providers
+{
+    public class appAuthorizationServerProvider:OAuthAuthorizationServerProvider
+    {
+        public async override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            return base.GrantResourceOwnerCredentials(context);
+        }
+    }
+}
